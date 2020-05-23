94701b6810ed232c099efc26c4c389e324200899
Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2020-05-23 14:54:33 +0200

    Test to stream DiffSquares from image

---
+++ RTL/src/test/scala/bm3d/DiffSquareUnit.scala
@@ -0,0 +1,88 @@
+// See README.md for license details.
+
+package duvs
+
+import java.io.File
+import scala.collection.mutable.ArrayBuffer
+import scala.language.reflectiveCalls
+import scala.language.implicitConversions
+
+import org.scalatest._
+
+import filters._
+
+import chisel3._
+import chisel3.experimental._
+
+import chiseltest._
+import chiseltest.internal._
+import chiseltest.experimental.TestOptionBuilder._
+
+import firrtl.options.TargetDirAnnotation
+import firrtl.{EmitCircuitAnnotation}
+
+import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
+import firrtl.{EmitAllModulesAnnotation}
+import firrtl.options.{TargetDirAnnotation}
+
+class StreamDiffSquareDriver(duv: StreamDiffSquare) {
+
+  private def init: Unit = {
+    duv.io.pixelLast.poke(false.B)
+    duv.io.pixel.bits.poke(0.U)
+    duv.io.pixel.valid.poke(false.B)
+  }
+
+  this.init
+
+  def runDUV(rows: Int, useAssert : Boolean): Unit = {
+    val pixelArray: Array[Int] = (for (
+      i <- 0 until rows;
+      j <- 0 until duv.width
+    ) yield (j*j + i*duv.width)).toArray
+
+    println("how many reads" + duv.maxBatchJumps*duv.wSize)
+    for(jump <- 0 until duv.maxBatchJumps*duv.wSize) {
+      duv.io.pixel.valid.poke(true.B)
+      for (
+        row <- 0 until rows;
+        col <- 0 until duv.width
+      ) {
+        val currPixel = row * duv.width + col
+        val currSquare = pixelArray(currPixel)
+        duv.io.pixel.bits.poke(currSquare.U)
+        if(row == rows-1 && col == duv.width-1) {
+          duv.io.pixelLast.poke(true.B)
+        }
+        duv.clock.step()
+      }
+      duv.io.pixel.valid.poke(false.B)
+      duv.io.pixelLast.poke(false.B)
+      duv.clock.step(3)
+    }
+  }
+}
+
+class StreamDiffSquareTester extends FlatSpec with ChiselScalatestTester {
+
+  val annos = Seq(
+    VerilatorBackendAnnotation
+    ,TargetDirAnnotation("test/diffSquares")
+    ,WriteVcdAnnotation)
+    //)
+
+  behavior of "duv DiffSquare"
+
+  //*
+  it should "return Difference Squared of Pixels" in {
+    test(new StreamDiffSquare(4, 10, 4)).withAnnotations(annos) { dut =>
+      val duvDrv = new StreamDiffSquareDriver(dut)
+      duvDrv.runDUV(6, true)
+
+      duvDrv.runDUV(6, true)
+
+    }
+  }
+  // */
+}
+
