b77e3dc590bd74dd4dafafce696d3fd3dc7c0358
Ulises Luzius <rafael.pizarrosolar@epfl.ch>
2020-05-26 19:40:15 +0200

    Uniform diff size, rid of window edges

--- RTL/src/main/scala/bm3d/StreamDiffSquare.scala
+++ RTL/src/main/scala/bm3d/StreamDiffSquare.scala
@@ -8,7 +8,7 @@ import chisel3.util._
 import modules._
 
 class StreamDiffSquare(
-  val wSize: Int = 4, // Size of search window 
+  val wSize: Int = 4, // Size of half search window 
   val width: Int = 720,
   val nbWorkers: Int = 2 // Nb of parallel workers
 ) extends MultiIOModule {
@@ -26,11 +26,11 @@ class StreamDiffSquare(
   val rowBuffer = Module(new FIFO()(new BRAMConfig(
     1, 9, bufferSize, "", false, true, false)))
 
-  val s_Buffer :: s_Stream :: Nil = Enum(2)
+  val s_Drop :: s_Stream :: s_Buffer :: Nil = Enum(3)
   val state = RegInit(s_Buffer)
 
   val currPixel = RegInit(0.U(log2Ceil(bufferSize).W))
-  val currStartOffstInit = nbWorkers.U(log2Ceil(bufferSize).W)
+  val currStartOffstInit = 0.U(log2Ceil(bufferSize).W)
   val currStartOffst = RegInit(currStartOffstInit)
   val (nbRowBatchJumps, isLastRowBatch) = {
     if(maxBatchJumps == 1) {
@@ -48,6 +48,8 @@ class StreamDiffSquare(
     val lastRow = row === (wSize-1).U
     (row, lastRow)
   }
+  val endBuffer = (wSize-1)*width + (wSize-1)
+  val rowSize = width - 2*wSize
 
   val jumpNextRow = currStartOffst + (width - wSize + nbWorkers).U
   val jumpNextBatch = currStartOffst + nbWorkers.U
@@ -55,36 +57,81 @@ class StreamDiffSquare(
   val flush = WireInit(false.B)
   val flushOnceStreamed = ShiftRegister(flush, rowBuffer.outDelay)
   rowBuffer.io.flush := flush
-  rowBuffer.io.enq.valid := io.pixel.valid
+  rowBuffer.io.enq.valid := io.pixel.valid && (state =/= s_Drop)
   rowBuffer.io.enq.bits  := Cat(io.pixelLast.asUInt, io.pixel.bits)
   val basePixel = rowBuffer.io.deq.bits(7,0)
   val lastPixel = rowBuffer.io.deq.bits(8).asBool
+  val currPixelNext = currPixel + 1.U
   rowBuffer.io.deq.ready := state === s_Stream
 
-  switch(state) {
-    is(s_Buffer) {
-      when(io.pixel.valid) {
-        currPixel := currPixel + 1.U
-        when(currPixel === currStartOffst) {
-          state := s_Stream
+
+  val dropFirst = true
+  when(io.pixel.valid) {
+    currPixel := currPixelNext
+  }
+  if(dropFirst) {
+    switch(state) {
+      is(s_Drop) {
+        when(io.pixel.valid) {
+          when(currPixelNext === currStartOffst) {
+            state := s_Buffer
+          }
+        }
+      }
+      is(s_Buffer) {
+        when(io.pixel.valid) {
+          when(currPixel === endBuffer.U) {
+            state := s_Stream
+          }
+        }
+      }
+
+      is(s_Stream) {
+        when(io.pixelLast) {
+          state := s_Drop
+          flush := true.B
+          currPixel := 0.U
+          currStartOffst := jumpNextBatch
+          nbRowBatchJumps := nbRowBatchJumps + 1.U
+          when(isLastRowBatch) {
+            nbRowBatchJumps := 0.U
+            nbRowJumps := nbRowJumps + 1.U
+            currStartOffst := jumpNextRow
+            when(isLastRow) {
+              state := s_Buffer
+              nbRowJumps := 0.U
+              currStartOffst := currStartOffstInit
+            }
+          }
         }
       }
     }
+  } else {
+    switch(state) {
+      is(s_Buffer) {
+        when(io.pixel.valid) {
+          currPixel := currPixel + 1.U
+          when(currPixel === currStartOffst) {
+            state := s_Stream
+          }
+        }
+      }
 
-    is(s_Stream) {
-      when(io.pixelLast) {
-        state := s_Buffer
-        flush := true.B
-        currPixel := 0.U
-        currStartOffst := jumpNextBatch
-        nbRowBatchJumps := nbRowBatchJumps + 1.U
-        when(isLastRowBatch) {
-          nbRowBatchJumps := 0.U
-          nbRowJumps := nbRowJumps + 1.U
-          currStartOffst := jumpNextRow
-          when(isLastRow) {
-            nbRowJumps := 0.U
-            currStartOffst := currStartOffstInit
+      is(s_Stream) {
+        when(io.pixelLast) {
+          state := s_Buffer
+          flush := true.B
+          currPixel := 0.U
+          currStartOffst := jumpNextBatch
+          nbRowBatchJumps := nbRowBatchJumps + 1.U
+          when(isLastRowBatch) {
+            nbRowBatchJumps := 0.U
+            nbRowJumps := nbRowJumps + 1.U
+            currStartOffst := jumpNextRow
+            when(isLastRow) {
+              nbRowJumps := 0.U
+              currStartOffst := currStartOffstInit
+            }
           }
         }
       }
@@ -106,6 +153,8 @@ class StreamDiffSquare(
     diffSquarer(chan).io.sub := pixelWorkers(chan).bits.zext.asSInt
     diffSquarer(chan).io.ce := true.B
     io.diffSquared(chan).bits := diffSquarer(chan).io.sumSquares
-    io.diffSquared(chan).valid := pixelWorkers(chan).valid || validDelayed
+    io.diffSquared(chan).valid := ShiftRegister(pixelWorkers(chan).valid, diffSquarer(chan).delay)
+    // io.diffSquared(chan).valid := ShiftRegister(pixelDelayed, diffSquarer(chan).delay)
+    // io.diffSquared(chan).valid := pixelWorkers(chan).valid // Buffer first
   }
 }
